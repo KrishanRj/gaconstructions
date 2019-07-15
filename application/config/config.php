d92b2245d584fadd5a4dcf8059bd78595d23b664
KrishanRj <30726173+KrishanRj@users.noreply.github.com>
2019-07-15 22:55:53 +0530

    Added Elastic IP to base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/gaconstructions/';
+$config['base_url'] = 'http://13.235.127.158/gaconstructions/';
 
 /*
 |--------------------------------------------------------------------------
